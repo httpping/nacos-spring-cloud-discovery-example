5b60c9664f4d9cd773f1dda989db1e9652b1cefc
tanping@globalegrow.com <tp123456>
2019-12-26 18:16:47 +0800

    加入了 zipkin 和  admin

--- nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-admin/src/main/java/com/test/admin/NacosAdminApplication.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-admin/src/main/java/com/test/admin/NacosAdminApplication.java
@@ -1,5 +1,6 @@
 package com.test.admin;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -28,5 +31,8 @@ public class NacosAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosAdminApplication.class, args);
     }
+
+
+
 
 }
